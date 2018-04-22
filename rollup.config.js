40fff556f18b507aff6f62a88090170b8cbf374a
Giovanni Silva <giovanni@atende.info>
2018-04-22 19:02:25 -0300

    update readme and package

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,32 @@
 import nodeResolve from 'rollup-plugin-node-resolve';
 export default {
-  entry: 'dist/index.js',
+  input: 'dist/src/index.js',
   plugins: [
     nodeResolve({
       
     })
   ],
-  dest: 'dist/bundles/angular-spa.umd.js',
-  sourceMap: false,
-  format: 'umd',
+  output: {
+    file: 'dist/src/bundles/angular-spa.umd.js',
+    sourceMap: false,
+    format: 'umd',
+    name: 'angularSpa',
+    globals: {
+      '@angular/core': 'ng.core',
+      '@angular/common': 'ng.common',
+      '@angular/http': 'ng.http',
+      '@angular/router': 'ng.router',
+      'angular-http-interceptor': 'angularHttpInterceptor',
+      'rxjs/Observable': 'Rx',
+      'rxjs/Subject': 'Rx',
+      'rxjs/add/operator/map': 'Rx.Observable.prototype',
+      'rxjs/add/operator/mergeMap': 'Rx.Observable.prototype',
+      'rxjs/add/observable/fromEvent': 'Rx.Observable',
+      'rxjs/add/observable/of': 'Rx.Observable'
+    }
+    
+  },
+  
   context: 'this',
   onwarn: function (warning) {
         // Suppress this error message... there are hundreds of them. Angular team says to ignore it.
@@ -18,7 +36,6 @@ export default {
         }
         console.error(warning.message);
     },
-  moduleName: 'angularSpa',
   external: [
     '@angular/core',
     '@angular/http',
@@ -29,18 +46,5 @@ export default {
     'rxjs/Observable',
     'rxjs/Subject',
     'rxjs/add/operator/mergeMap'
-  ],
-  globals: {
-    '@angular/core': 'ng.core',
-    '@angular/common': 'ng.common',
-    '@angular/http': 'ng.http',
-    '@angular/router': 'ng.router',
-    'angular-http-interceptor': 'angularHttpInterceptor',
-    'rxjs/Observable': 'Rx',
-    'rxjs/Subject': 'Rx',
-    'rxjs/add/operator/map': 'Rx.Observable.prototype',
-    'rxjs/add/operator/mergeMap': 'Rx.Observable.prototype',
-    'rxjs/add/observable/fromEvent': 'Rx.Observable',
-    'rxjs/add/observable/of': 'Rx.Observable'
-  }
+  ]
 }
